a80ab8b36855ecd0f05820ea082b8304b31f0931
gcolind <gcolin@pdx.edu>
2014-05-29 13:34:53 -0700

    Slight alterations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Javascript for Pomodori 
 // Problem: How to display a timer 20 - 30 minutues (as assigned by the user) / 5 - 15 break timer
 
+var time; // Variable to hold displayable time
 var minutes; // variable to hold minutes
 var seconds; // variable to hold seconds
 
