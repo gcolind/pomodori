cc62820ec61d75d697ede4fd9ae766bfbc250751
gcolind <gcolin@pdx.edu>
2014-05-29 13:12:44 -0700

    added javascript basic description comment / some code

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Javascript for Pomodori 
+// Problem: How to display a timer 20 - 30 minutues (as assigned by the user) / 5 - 15 break timer
+
+var minutes; // variable to hold minutes
+var seconds; // variable to hold seconds
+
+// From user input, gather the time in seconds
+function getTimeSeconds(uinput){
+	minutes = uinput;
+	seconds = uinput * 60; // Formated time, to be returned by the function (user input (which is in minutes) * 60 to get seconds)
+	return seconds;
+}
